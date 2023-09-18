befd62b91121868f180302bbaf0bc306b751d1b0
LongWay1962 <1164819954@qq.com>
2023-09-18 19:00:40 +0800

    修改解决冲突

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         System.out.println("HelloWadsfasdforld!");
         System.out.println("Helloq我是老二，吃了又睡sdforld!");
+        System.out.println("He我是老大 ffoWorld!");
         System.out.println("HellasdfoWorld!");
 	}
 }
